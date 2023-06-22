dee78484a2511275692da3329195d34d7a66667a
Octo8080 <okutannderuta0529@gmail.com>
2023-06-23 01:01:30 +0900

    update document

--- routes/[...slug].tsx
+++ routes/[...slug].tsx
@@ -65,11 +65,12 @@ export default await function SlagPage(props: PageProps<Page & {toc: Toc}>) {
   return (
     <div class="max-w-screen-lg flex flex-col gap-3">
       <Head>
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css" integrity="sha512-Ya9H+OPj8NgcQk34nCrbehaA0atbzGdZCI2uCbqVRELgnlrh8vQ2INMnkadVMSniC54HChLIh5htabVuKJww8g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-twilight.min.css"></link>
         <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-bash.js"></script>
-
       </Head>
       <ModalMounter class="md:hidden" />
       <div class="flex flex-row gap-3">
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,6 +4,12 @@ import Hero from "../components/Hero.tsx";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Brick Wall Bata</title>
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
       <div class="p-4">
         <Hero />
       </div>
