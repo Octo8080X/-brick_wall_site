5a97502b2dd6e2eda584f010dc65af04a1781d9b
Octo8080 <okutannderuta0529@gmail.com>
2023-06-23 01:08:17 +0900

    update document

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component }: AppProps) {
       <Head>
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image" content="https://brickwall.deno.dev/brick_wall_logo.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css" integrity="sha512-Ya9H+OPj8NgcQk34nCrbehaA0atbzGdZCI2uCbqVRELgnlrh8vQ2INMnkadVMSniC54HChLIh5htabVuKJww8g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-twilight.min.css"></link>
